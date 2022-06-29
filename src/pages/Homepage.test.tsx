890cf747fbf7bf3b12bf76b3d2fc960e34f9b899
HENG Kaniha <hengkaniha@gmail.com>
2022-06-29 09:47:39 +0200

    testing homepage

--- src/pages/Homepage.test.tsx
+++ src/pages/Homepage.test.tsx
@@ -1,20 +1,26 @@
 import {fireEvent, render, screen} from '@testing-library/react'
-// import {createMemoryHistory} from 'history'
-// import Homepage from './Homepage'
-// import SignUp from './SignUp'
+import { MemoryRouter } from 'react-router-dom'
+import Homepage from './Homepage'
+import SignUp from './SignUp'
 
 test('When user arrive in the homepage', () => {
-    // render(
-    //     <Homepage />
-    // )
-    // expect(screen.getByRole('button')).toBeInTheDocument()
-    // expect(screen.getByText(/Login/i)).toBeInTheDocument()
+    render(
+        <MemoryRouter>
+            <Homepage />
+        </MemoryRouter>
+    )
+    expect(screen.getByRole('button')).toBeInTheDocument()
+    expect(screen.getByText(/Login/i)).toBeInTheDocument()
 
-    // describe('When user clicks sign up button', () => {
-    //     it('go to signup page', () => {
-    //       render(<SignUp />)
-    //       fireEvent.click(screen.getByRole('button'))
-    //       expect(screen.getByText(/Sign up/i)).toBeInTheDocument()
-    //     })
-    //   })
+    describe('When user clicks sign up button', () => {
+        it('go to signup page', () => {
+            render(
+                <MemoryRouter>
+                    <SignUp />
+                </MemoryRouter>
+            )
+            fireEvent.click(screen.getByRole('button'))
+            expect(screen.getByText(/Sign up/i)).toBeInTheDocument()
+        })
+    })
 })
